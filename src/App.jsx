f7feb90313607630e719e1dee22153056aefc1b6
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-19 13:21:30 +0000

    set up the routes of the pages of log in and products

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Products from './pages/Products';
+import Login from './pages/Login'; 
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          {/* Automatically redirect the home page to the login screen */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
