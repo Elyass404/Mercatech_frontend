090cb7065c37f8203f8fced45353451f1dbfc52d
ilyass mar <ilyass.marghine22@gmail.com>
2026-02-19 13:22:19 +0000

    created the api file to declare the api url

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+// src/services/api.js
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080/api/v1', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true
+});
+
+export default api;
